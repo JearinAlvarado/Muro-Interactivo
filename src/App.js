6322754523f76ba4863fb8f753fa75dab6dcc684
jearinAlvarado <jearinalvarado110403@gmail.com>
2023-04-10 21:07:41 -0500

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from "react-router-dom"
+import {Home} from './pages/Home';
+import {Register} from './pages/Authentication/Register';
+import {Login} from  './pages/Authentication/Login'
+import {AuthProvider} from './authContext'
+import {RecoverPassword} from './pages/Authentication/RecoverPassword'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={
+          <Home />
+        }/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/recoverpassword' element={<RecoverPassword/>}/>
+
+      </Routes>
+      </AuthProvider>
     </div>
   );
 }
